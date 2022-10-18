9e607bc9f2c71115e80fca9d5ffb94cde178bc02
rreskey <smivasia@gmail.com>
2022-10-18 16:45:03 +0300

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid='md' className='machine'>
+      <Row>
+        <Col className='drum'>
+          <p className='sound'>Q</p>
+        </Col>
+        <Col className='drum'>
+          <p className='sound'>W</p>
+        </Col>
+        <Col className='drum'>
+          <p className='sound'>E</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col className='drum'>
+          <p className='sound'>A</p>
+        </Col>
+        <Col className='drum'>
+          <p className='sound'>S</p>
+        </Col>
+        <Col className='drum'>
+          <p className='sound'>D</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col className='drum'>
+          <p className='sound'>Z</p>
+        </Col>
+        <Col className='drum'>
+          <p className='sound'>X</p>
+        </Col>
+        <Col className='drum'>
+          <p className='sound'>C</p>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
